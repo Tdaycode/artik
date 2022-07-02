4b2be0b27c9fc45280a0d108095da9340f44fd3d
Omotayo Ganiyu Seedik <60110209+Tdaycode@users.noreply.github.com>
2022-07-03 00:19:19 +0100

    updating user

--- models/job.model.js
+++ models/job.model.js
@@ -1,87 +1,48 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const jobSchema = mongoose.Schema(
-//   {
-//     postedBy: {
-//       type: Schema.Types.ObjectId,
-//       ref: 'User'
-//     },
-//     lastName: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         default: '',
-//     },
-//     city: {
-//       type: String,
-//       required: true,
-//       trim: true,
-//       default: 'current location',
-//   },
-//    state: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//     default: 'state',
-// },
-//   phone: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       trim: true,
-//       default: '+234',
-//     },
-//     isArtisan: {
-//       type: Boolean,
-//       default: false,
-//     },
-//     skill:{
-//       type: String,
-//       default: '',      
-//     },
-//     experience: {
-//       type: String,
-//       default: '',
-//     },
-//     workshopAddress: {
-//       type: String,
-//       default: '',
-//     },
-//     email: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       trim: true,
-//       lowercase: true,
-//       validate(value) {
-//         if (!validator.isEmail(value)) {
-//           throw new Error('Invalid email');
-//         }
-//       },
-//     },
-//     password: {
-//       type: String,
-//       required: true,
-//       trim: true,
-//       minlength: 8,
-//       validate(value) {
-//         if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
-//           throw new Error('Password must contain at least one letter and one number');
-//         }
-//       }
-//     }
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
+const jobSchema = mongoose.Schema(
+  {
+    postedBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    jobDetails: {
+        type: String,
+        required: true,
+        trim: true,
+        default: '',
+    },
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+      default: 'current location',
+  },
+   cost: {
+    type: Number,
+    required: true,
+    trim: true,
+    default: 0,
+},
+  deadline: {
+      type: Date,
+      required: true,
+      unique: true,
+      default: Date.now(),
+    },
+   
+  },
+  {
+    timestamps: true,
+  }
+);
 
 
 
 
-// /**
-//  * @typedef Job
-//  */
-// const User = mongoose.model('User', userSchema);
+/**
+ * @typedef Job
+ */
+const Job = mongoose.model('Job', jobSchema);
 
-// module.exports = User;
+module.exports = User;
